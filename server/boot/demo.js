4e97f82fadf565b95c3f56ab2ddf1dce676c21cd
JioxoFab <fschenker@windowslive.com>
2016-05-01 12:28:49 +0200

    add demo data

--- server/boot/demo.js
+++ server/boot/demo.js
@@ -1,9 +1,19 @@
 module.exports = function(app) {
-	// Add a restaurant
+	// Add restaurants
 	var Restaurant = app.models.Restaurant;
 	Restaurant.create({
 		"id": 1,
-		"name": "test",
-		"location": "Here"
+		"name": "Charlie",
+		"location": "Zurich, Switzerland"
+	}, function(err, obj) {console.log(obj);});
+	Restaurant.create({
+		"id": 2,
+		"name": "Brown",
+		"location": "Zurich, Switzerland"
+	}, function(err, obj) {console.log(obj);});
+	Restaurant.create({
+		"id": 3,
+		"name": "Juan",
+		"location": "Zurich, Switzerland"
 	}, function(err, obj) {console.log(obj);});
 }
